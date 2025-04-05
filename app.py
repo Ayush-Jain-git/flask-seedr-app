9cf518718be94b61b45db8d27a8ecbe01656d977
Ayush-Jain-git <76630124+Ayush-Jain-git@users.noreply.github.com>
2025-04-05 17:56:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,8 @@ def upload_to_seedr(magnet_link):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--remote-debugging-port=9222")  
+    chrome_options.add_argument("--single-process")
+    chrome_options.add_argument("--disable-software-rasterizer") 
     chrome_options.binary_location = "/usr/bin/google-chrome" 
     service = Service("/usr/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=chrome_options)
@@ -137,9 +138,13 @@ def upload_magnet():
     magnet_link = "magnet:?xt=urn:btih:C5A1A41B6AE10D5F4F3FCDF3042F2F396A5DDCF8&dn=DaniDaniels+-+Dani+Daniels+-+School+Girl+POV+Sex+And+Dick+Ride&tr=http%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2780%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2730%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce"
     if not magnet_link:
         return jsonify({"error": "Magnet link is required"}), 400
+    
+    def background_upload():
+        result = upload_to_seedr(magnet_link)
+        print("Background upload finished:", result)
 
-    result = upload_to_seedr(magnet_link)
-    return jsonify({"message": result})
+    threading.Thread(target=background_upload).start()
+    return jsonify({"message": "Upload started in background."})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
