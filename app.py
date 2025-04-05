eeb2d789df2542e22b86b354a20bc73f7f644452
Ayush-Jain-git <76630124+Ayush-Jain-git@users.noreply.github.com>
2025-04-05 19:06:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,12 +29,15 @@ def get_magnet_link(search_query):
         return None
 
     soup = BeautifulSoup(response.text, "html.parser")
-    first_result = soup.select_one(".table-list tbody tr td.name a[href^='/torrent/']")
-    
-    if not first_result:
+    first_row = soup.select_one(".table-list tbody tr")
+    if not first_row:
         return None
 
-    torrent_page_url = "https://www.1337x.to" + first_result["href"]
+    torrent_links = first_row.select("td.name a[href^='/torrent/']")
+    if len(torrent_links) < 2:
+        return None
+
+    torrent_page_url = "https://www.1337x.to" + torrent_links[1]["href"]
     response = requests.get(torrent_page_url, headers=headers)
     
     if response.status_code != 200:
@@ -141,7 +144,6 @@ def upload_to_seedr(magnet_link):
 def upload_magnet():
     data = request.json
     magnet_link = data.get("magnet")
-    magnet_link = "magnet:?xt=urn:btih:C5A1A41B6AE10D5F4F3FCDF3042F2F396A5DDCF8&dn=DaniDaniels+-+Dani+Daniels+-+School+Girl+POV+Sex+And+Dick+Ride&tr=http%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2780%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2730%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce"
     if not magnet_link:
         return jsonify({"error": "Magnet link is required"}), 400
     
